331d7a73d62e7761a64f7e7e5f7436029d500c06
MEZ <mez@bitwormhole.com>
2023-02-28 07:54:10 +0800

    更新了静态 web 资源

---
+++ src/main/resources/static/js/299.bc8ecb8e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkwpm_station_vue"]=self["webpackChunkwpm_station_vue"]||[]).push([[299],{5237:function(n,t,e){e.r(t),e.d(t,{default:function(){return o}});var u=e(3396);function s(n,t,e,s,r,a){const i=(0,u.up)("SettingsIndex"),c=(0,u.up)("AdminFrame");return(0,u.wg)(),(0,u.j4)(c,null,{default:(0,u.w5)((()=>[(0,u.Wm)(i)])),_:1})}var r=e(3775),a={name:"Settings",components:{SettingsIndex:r.Z}},i=e(89);const c=(0,i.Z)(a,[["render",s]]);var o=c}}]);
+//# sourceMappingURL=299.bc8ecb8e.js.map
